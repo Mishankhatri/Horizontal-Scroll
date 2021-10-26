f3aa62d2bfe15e8a87e9a8bcf31dc3a6fd1c4e97
Mishan khatri <mishankhatri490@gmail.com>
2021-10-27 00:01:11 +0545

    scroll experiments

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
+const nav_links = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+console.log(nav_links);
 
-window.addEventListener('keydown', function(event) { 
-    const key = event.key; 
-    switch (event.key) {
-        case "ArrowLeft":
-            window.scrollBy(-window.innerWidth,0);
-            break;
-        case "ArrowRight":
-            window.scrollBy(window.innerWidth,0);
-            break;
-    }
+const back_button = document.querySelector("button.back");
+const next_button = document.querySelector("button.next");
+
+window.addEventListener("keydown", function (event) {
+  const key = event.key;
+  switch (event.key) {
+    case "ArrowLeft":
+      window.scrollBy(-window.innerWidth, 0);
+      break;
+
+    case "ArrowRight":
+      window.scrollBy(window.innerWidth, 0);
+      break;
+    case "ArrowUp":
+      window.scrollBy(-window.innerWidth, 0);
+      break;
+    case "ArrowDown":
+      window.scrollBy(window.innerWidth, 0);
+      break;
+  }
 });
+
+back_button.addEventListener("click", ()=>{
+  window.scrollBy(-window.innerWidth, 0);
+})
+next_button.addEventListener("click", ()=>{
+  window.scrollBy(window.innerWidth, 0);
+})
+
+
+for (i = 0; i < nav_links.length; i++) {
+  nav_links[i].onclick = function () {
+    var f = document.querySelector("nav ul li > a.active");
+    if (f) {
+      f.classList.remove("active");
+    }
+    this.classList.add("active");
+  };
+}
+
+
+
+window.addEventListener('scroll',(e)=>{
+  let current = '';
+  sections.forEach( section => {
+    const sectionTop = section.offsetLeft;
+    const sectionwidth = section.clientWidth;
+    if(pageXOffset >= sectionTop){
+      current = section.getAttribute('id');
+    }
+  });
+  console.log(current);
+  nav_links.forEach( li =>{
+    li.classList.remove('active');
+    if(li.classList.contains(current)) li.classList.add("active");
+  });
+  
+})
